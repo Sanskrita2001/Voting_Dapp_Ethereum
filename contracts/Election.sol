bdc2eb77d42b2f0d517b52e575a693758adc2143
Sanskrita Saha <sanskrita2001@gmail.com>
2021-02-20 01:46:37 +0530

    Working App

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,9 +7,21 @@ contract Election{
         string _name;
         uint voteCount;
     }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+
      mapping(uint => Candidate) public candidates;
     // Store Candidates Count
     uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
     //constructor
     constructor  () public{
         addCandidate("Candidate 1");
@@ -18,5 +30,21 @@ contract Election{
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
